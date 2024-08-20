895c839d50bdb100aed3f478e619f88f454abd4e
Astro <astrocket.dev@gmail.com>
2024-08-20 21:23:11 +0900

    edit html tag

--- lib/ahoy_email/processor.rb
+++ lib/ahoy_email/processor.rb
@@ -61,7 +61,7 @@ module AhoyEmail
         part = message.html_part || message
         raw_source = part.body.raw_source
 
-        regex = /<body>/i
+        regex = /<\/body>/i
         url =
           url_for(
             controller: "ahoy/messages",
